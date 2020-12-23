988ff26db0bfa007bc7467b07b26e263a05ab8d6
bob <elapso@yandex.ru>
2020-12-23 22:43:05 +0300

    init import

---
+++ markers/views.py
@@ -0,0 +1,20 @@
+import json
+
+from django.core.serializers import serialize
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+from .models import Marker
+
+# Create your views here.
+class MarkersMapView(TemplateView):
+    """Markers map view."""
+
+    template_name = "map.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["markers"] = json.loads(serialize('geojson', Marker.objects.all()))
+        return context
+
+
